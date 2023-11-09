5a9096105ba45a48010c5c84a3df5913024b9fde
Vito Jak <93348449+Victino@users.noreply.github.com>
2023-11-09 23:01:22 +0100

    mazegame

---
+++ Assets/Scripts/MazeRotationScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MazeRotationScript : MonoBehaviour
+{
+    [SerializeField] private float MinXRot = -10f;
+    [SerializeField] private float MaxXRot = 10f;
+    [SerializeField] private float MinZRot = -10f;
+    [SerializeField] private float MaxZRot = 10f;
+    [SerializeField] private float RotationSpeed = 0.5f;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+    }
+}
