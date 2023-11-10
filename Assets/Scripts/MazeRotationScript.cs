f37cce16d4c1355e8f7298f6880d23b24d5c1286
Vito Jak <vito.jak@hva.nl>
2023-11-10 14:53:19 +0100

    rotation clamping

--- Assets/Scripts/MazeRotationScript.cs
+++ Assets/Scripts/MazeRotationScript.cs
@@ -1,26 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MazeRotationScript : MonoBehaviour
 {
-    [SerializeField] private float MinXRot = -10f;
-    [SerializeField] private float MaxXRot = 10f;
-    [SerializeField] private float MinZRot = -10f;
-    [SerializeField] private float MaxZRot = 10f;
-    [SerializeField] private float RotationSpeed = 0.5f;
+    [SerializeField] private int MinRotation;
+    [SerializeField] private int MaxRotation;
 
+    private float CurrentXRotation;
+    private float CurrentZRotation;
+    private Vector3 startingPosition;
 
-    void Start()
+    private void Start()
     {
-
+        startingPosition = transform.position;
     }
 
+    // Update is called once per frame
     void Update()
     {
+        Time.timeScale = 5f;
 
+        CurrentXRotation = transform.eulerAngles.x >= 180 ? transform.eulerAngles.x - 360 : transform.eulerAngles.x;
+        CurrentZRotation = transform.eulerAngles.z >= 180 ? transform.eulerAngles.z - 360 : transform.eulerAngles.z;
+
+        if (CurrentZRotation >= MaxRotation|| CurrentZRotation <= MinRotation)
+        {
+            ClampZRotation();
+        }
+
+        if (CurrentXRotation >= MaxRotation || CurrentXRotation <= MinRotation)
+        {
+            ClampXRotation(); 
+        } 
     }
 
-    void OnCollisionEnter(Collision collision)
+    private void FixedUpdate()
     {
+        transform.position = startingPosition;
+    }
 
+    private void ClampZRotation() 
+    {
+        CurrentZRotation = Mathf.Clamp(CurrentZRotation, MinRotation, MaxRotation);
+        transform.rotation = Quaternion.Euler(CurrentXRotation, 0, CurrentZRotation);
+    }
+
+    private void ClampXRotation()
+    {
+        CurrentXRotation = Mathf.Clamp(CurrentXRotation, MinRotation, MaxRotation);
+        transform.rotation = Quaternion.Euler(CurrentXRotation, 0, CurrentZRotation);
     }
 }
