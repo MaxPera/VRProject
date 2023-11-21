bb8b805f80d16e8e0a5ffdd35c321aed65a1e100
MaxPera <MaxPera666@gmail.com>
2023-11-21 15:49:51 +0100

    MazeSFX

--- Assets/Scripts/Audio/CollisionEmitter.cs
+++ Assets/Scripts/Audio/CollisionEmitter.cs
@@ -1,16 +1,8 @@
 using System.Collections;
 using UnityEngine;
-using FMOD.Studio;
-using FMODUnity;
 
-public class CollisionEmitter : SoundEmitter
+public class CollisionEmitter : VelocityEmitter
 {
-    [SerializeField]
-    private float cooldownTimer;
-    private bool canPlay = true;
-    private bool hasStarted = false;
-    [SerializeField]
-    private bool usesVelocity;
 
     private void OnEnable()
     {
@@ -21,24 +13,6 @@ public class CollisionEmitter : SoundEmitter
     {
         yield return new WaitForSeconds(cooldownTimer);
         hasStarted = true;
-    }
-
-    private IEnumerator PlaySound()
-    {
-        if (usesVelocity)
-        {
-            if (!TryGetComponent(out Rigidbody rBody))
-                Debug.LogError($"No rigidbody on{gameObject.name}");
-            if (!TryGetComponent(out StudioEventEmitter eventEmitter))
-                Debug.LogError($"No StudioEventEmitter on{gameObject.name}");
-            EventInstance eventInstance = eventEmitter.EventInstance;
-            float volumeVelocity = Mathf.Clamp01(rBody.velocity.magnitude * 50f);
-
-            AudioManager.instance.SetEventInstanceParameter(eventInstance, "VolumeVelocity", volumeVelocity);
-        }
-        canPlay = false;
-        yield return new WaitForEndOfFrame();
-        AudioManager.instance.PlayEmitter(gameObject);
     }
 
     private IEnumerator StartTimer()
@@ -52,7 +26,7 @@ public class CollisionEmitter : SoundEmitter
         if (collision.gameObject.GetComponent<Collider>() && hasStarted && !canPlay)
         {
             StartCoroutine(StartTimer());
-            if(canPlay == true)
+            if (canPlay == true)
                 StartCoroutine(PlaySound());
         }
     }
---
+++ Assets/Scripts/Audio/VelocityEmitter.cs
@@ -0,0 +1,43 @@
+using FMOD.Studio;
+using FMODUnity;
+using System.Collections;
+using UnityEngine;
+
+public class VelocityEmitter : SoundEmitter
+{
+    [SerializeField]
+    protected float cooldownTimer;
+    protected bool canPlay = true;
+    protected bool hasStarted = false;
+    [SerializeField]
+    protected bool usesVelocity;
+
+    private void OnEnable()
+    {
+        StartCoroutine(SetBool());
+    }
+
+    private IEnumerator SetBool()
+    {
+        yield return new WaitForSeconds(cooldownTimer);
+        hasStarted = true;
+    }
+
+    protected IEnumerator PlaySound()
+    {
+        if (usesVelocity)
+        {
+            if (!TryGetComponent(out Rigidbody rBody))
+                Debug.LogError($"No rigidbody on{gameObject.name}");
+            if (!TryGetComponent(out StudioEventEmitter eventEmitter))
+                Debug.LogError($"No StudioEventEmitter on{gameObject.name}");
+            EventInstance eventInstance = eventEmitter.EventInstance;
+            float volumeVelocity = Mathf.Clamp01(rBody.velocity.magnitude * 50f);
+
+            AudioManager.instance.SetEventInstanceParameter(eventInstance, "VolumeVelocity", volumeVelocity);
+        }
+        canPlay = false;
+        yield return new WaitForEndOfFrame();
+        AudioManager.instance.PlayEmitter(gameObject);
+    }
+}
