496d2a21c791a00179cfdbdb07dafdf6912568e5
LeonLauran <leonlauran@gmail.com>
2023-11-24 11:45:05 +0100

    Changes

--- Assets/Scripts/MazeRotationScript.cs
+++ Assets/Scripts/MazeRotationScript.cs
@@ -17,22 +17,18 @@ public class MazeRotationScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Time.timeScale = 5f;
-
         CurrentXRotation = transform.eulerAngles.x >= 180 ? transform.eulerAngles.x - 360 : transform.eulerAngles.x;
         CurrentZRotation = transform.eulerAngles.z >= 180 ? transform.eulerAngles.z - 360 : transform.eulerAngles.z;
 
-        if (CurrentZRotation >= MaxRotation|| CurrentZRotation <= MinRotation)
-        {
+        if(CurrentZRotation >= MaxRotation || CurrentZRotation <= MinRotation) {
             CurrentZRotation = Mathf.Clamp(CurrentZRotation, MinRotation, MaxRotation);
             transform.rotation = Quaternion.Euler(CurrentXRotation, 0, CurrentZRotation);
         }
 
-        if (CurrentXRotation >= MaxRotation || CurrentXRotation <= MinRotation)
-        {
+        if(CurrentXRotation >= MaxRotation || CurrentXRotation <= MinRotation) {
             CurrentXRotation = Mathf.Clamp(CurrentXRotation, MinRotation, MaxRotation);
             transform.rotation = Quaternion.Euler(CurrentXRotation, 0, CurrentZRotation);
-        } 
+        }
     }
 
     private void FixedUpdate()
