386e056450d485463d1bc552cd913ffd5988c676
jonah Hoekstra <jonah.hoekstra@hva.nl>
2023-12-15 10:35:15 +0100

    Final feedback

--- Assets/Scripts/BalloonController.cs
+++ Assets/Scripts/BalloonController.cs
@@ -1,38 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Filtering;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(XRGrabInteractable))]
 public class BalloonController : MonoBehaviour
 {
-    public Rigidbody rb;
+    private Rigidbody rb;
     private Vector3 targetAngle = new Vector3(0, 0, 90f);
     private Vector3 currentAngle;
     private bool isAttached = false;
-    private float force = 15f;
-    private XRGrabInteractable grabController; 
 
     [SerializeField]
-    private float timeScale;
+    private float force = 15f;
+    private XRGrabInteractable grabController;
+
+    [SerializeField]
+    private float lerpSpeed;
 
     private void Start()
     {
-        grabController = GetComponent<XRGrabInteractable>(); 
+        grabController = GetComponent<XRGrabInteractable>();
+        rb = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(new Vector3(0, 1, 0) * force);
+        rb.AddForce(Vector3.up * force);
         currentAngle = transform.eulerAngles;
 
         if (isAttached)
         {
             currentAngle = new Vector3(
-                Mathf.LerpAngle(currentAngle.x, targetAngle.x, Time.deltaTime * timeScale),
-                Mathf.LerpAngle(currentAngle.y, targetAngle.y, Time.deltaTime * timeScale),
-                Mathf.LerpAngle(currentAngle.z, targetAngle.z, Time.deltaTime * timeScale));
+                Mathf.LerpAngle(currentAngle.x, targetAngle.x, lerpSpeed),
+                Mathf.LerpAngle(currentAngle.y, targetAngle.y, lerpSpeed),
+                Mathf.LerpAngle(currentAngle.z, targetAngle.z, lerpSpeed));
 
             transform.eulerAngles = currentAngle;
         }
@@ -42,17 +43,13 @@ public class BalloonController : MonoBehaviour
     {
         if (other.gameObject.tag == "Target")
         {
-            other.gameObject.SetActive(false);
-            HandleCollision();
+            isAttached = true;
+            rb.useGravity = false;
+            rb.isKinematic = true;
+            //Disable grabbing after collision
+            grabController.enabled = false;
+            //So this code only runs once
+            other.gameObject.tag = "Untagged";
         }
-    }
-
-    private void HandleCollision()
-    {
-        isAttached = true;
-        rb.useGravity = false;
-        rb.isKinematic = true;
-        //Disable grabbing after collision
-        grabController.enabled = false; 
     }
 }
