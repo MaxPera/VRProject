94abb3f77274d567bfa8474f5ca2062345f4ed1c
Jonah Hoekstra <jonah.hoekstra@hva.nl>
2023-12-15 09:54:12 +0000

    Merged PR 60: Malak interaction
    ##What changed?
    - Added a scene to test the Malak interaction.
    - A red helium balloon that floats.
    - A target cube that represents the child holding out his hand.
    
    ##How to test?
    - Load the scene.
    - Grab the balloon from the ceiling.
    - Let it go, watch the balloon float up.
    - Touch the balloon to the cube, it will snap in a fixed position and disable grabbing.
    
    Related work items: #146

---
+++ Assets/Scripts/BalloonController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(XRGrabInteractable))]
+public class BalloonController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 targetAngle = new Vector3(0, 0, 90f);
+    private Vector3 currentAngle;
+    private bool isAttached = false;
+
+    [SerializeField]
+    private float force = 15f;
+    private XRGrabInteractable grabController;
+
+    [SerializeField]
+    private float lerpSpeed;
+
+    private void Start()
+    {
+        grabController = GetComponent<XRGrabInteractable>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(Vector3.up * force);
+        currentAngle = transform.eulerAngles;
+
+        if (isAttached)
+        {
+            currentAngle = new Vector3(
+                Mathf.LerpAngle(currentAngle.x, targetAngle.x, lerpSpeed),
+                Mathf.LerpAngle(currentAngle.y, targetAngle.y, lerpSpeed),
+                Mathf.LerpAngle(currentAngle.z, targetAngle.z, lerpSpeed));
+
+            transform.eulerAngles = currentAngle;
+        }
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Target")
+        {
+            isAttached = true;
+            rb.useGravity = false;
+            rb.isKinematic = true;
+            //Disable grabbing after collision
+            grabController.enabled = false;
+            //So this code only runs once
+            other.gameObject.tag = "Untagged";
+        }
+    }
+}
