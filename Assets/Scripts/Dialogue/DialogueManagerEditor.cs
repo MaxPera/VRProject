1a317e8cbb1ee11a4d8a9bcb93bf8f24d845075b
LeonLauran <leonlauran@gmail.com>
2023-12-28 17:16:03 +0100

    Added dialogue manager editor script. Added dutch dialogue file.

---
+++ Assets/Scripts/Dialogue/DialogueManagerEditor.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEditor;
+
+[CustomEditor(typeof(DialogueManager))]
+public class DialogueManagerEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		DialogueManager dialogueManager = (DialogueManager)target;
+
+		if(dialogueManager.DialogueFiles.Length > 0) {
+			if(dialogueManager.ChosenDialogueFile == null) {
+				dialogueManager.ChosenDialogueFile = dialogueManager.DialogueFiles[0];
+			}
+
+			string[] dialogueFileNames = Array.ConvertAll(dialogueManager.DialogueFiles, file => file.name);
+
+			int chosenIndex = EditorGUILayout.Popup("Select language:", Array.IndexOf(dialogueFileNames, dialogueManager.ChosenDialogueFile.name), dialogueFileNames);
+			dialogueManager.ChosenDialogueFile = dialogueManager.DialogueFiles[chosenIndex];
+		}
+	}
+}
