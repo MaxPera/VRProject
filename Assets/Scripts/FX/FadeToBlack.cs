ca6d885ca0f671a291c7df833ef1f0cb3384fbf8
jonah Hoekstra <jonah.hoekstra@hva.nl>
2024-01-16 12:06:41 +0100

    Fade from main scene to ending scene

--- Assets/Scripts/FX/FadeToBlack.cs
+++ Assets/Scripts/FX/FadeToBlack.cs
@@ -11,44 +11,29 @@ public class FadeToBlack : MonoBehaviour
     private float _fadeTime = 5.0f;
     //Increases or decreases the duration of the fading effect. 
     [SerializeField]
-    [Range(0.1f, 1.0f)]
-    private float timeScale = 1.0f;
+    private float timeMultiplier = 1.0f;
     private Material _material;
-    private bool _isFadingOut = false;
 
     void Start()
     {
         _material = GetComponent<MeshRenderer>().material;
-        Fade(true);
     }
-    /// <summary>
-    /// Fade the players view to black or fade-in from black. 
-    /// </summary>
-    /// <param name="fadeOut">Set true to fade to black, false to fade in.</param>
-    void Fade(bool fadeOut)
-    {
-        //If we've already faded to black, we cannot fade out again. 
-        if (fadeOut && _isFadingOut)
-            return;
-
-        _isFadingOut = fadeOut;
-        StopAllCoroutines();
-        StartCoroutine(PlayEffect(fadeOut));
-    }
-
-    private IEnumerator PlayEffect(bool fadeOut)
+    public IEnumerator PlayEffect(bool fadeOut)
     {
         float startAlpha = _material.GetFloat("_Alpha");
         float endAlpha = fadeOut ? 1.0f : 0.0f;
-
         float elapsedTime = 0.0f;
-        while (elapsedTime < _fadeTime)
-        {
-            elapsedTime += Time.deltaTime * timeScale;
-            float alpha = Mathf.Lerp(startAlpha, endAlpha, elapsedTime);
+        float alpha = startAlpha; 
+
+        while (alpha != endAlpha)
+        {           
+            alpha = Mathf.Lerp(startAlpha, endAlpha, elapsedTime);
             _material.SetFloat("_Alpha", alpha);
+            Debug.Log(alpha);
+            elapsedTime += Time.deltaTime * timeMultiplier;
             yield return null;
         }
+
         //Ensure the alpha is always either 0 or 1. 
         _material.SetFloat("_Alpha", endAlpha);
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using System;
+using UnityEngine.Rendering;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public FadeToBlack fadeEffect; 
+    private bool hasFaded = false;
 
     private void Awake()
     {
@@ -16,13 +20,38 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(instance);
     }
 
-    public IEnumerator LoadNextSceneAsync(string scene)
+    private void Start()
+    {
+        EventBus.Instance.Subscribe<FadeOutEvent>(HandleLoad);
+        EventBus.Instance.SendEvent(this, new FadeOutEvent());
+    }
+
+    public void StartFade() 
+    {
+        EventBus.Instance.SendEvent(this, new FadeOutEvent()); 
+    }
+
+    private void HandleLoad(object sender, EventArgs args) 
+    {
+        StartCoroutine(LoadNextSceneAsync("EndingScene", true));      
+    }
+
+    public IEnumerator LoadNextSceneAsync(string scene, bool fadeOut)
     {
         Scene currentScene = SceneManager.GetActiveScene();
+        yield return HandleFade(fadeOut);
         AsyncOperation asyncOp = SceneManager.LoadSceneAsync(scene);
         yield return new WaitUntil(() => asyncOp.isDone == true);
         SceneManager.UnloadSceneAsync(currentScene);
 
     }
 
+    private IEnumerator HandleFade(bool fadeOut) 
+    {
+        if (hasFaded && fadeOut) yield return null;
+        //if (!hasFaded && !fadeOut) yield return null;
+        yield return fadeEffect.PlayEffect(fadeOut);
+        hasFaded = fadeOut;
+        yield return new WaitForSeconds(1.5f); 
+    }
 }
