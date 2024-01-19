c9e0adf5d016a75bbf14639e59e030223c1de45c
LeonLauran <leonlauran@gmail.com>
2024-01-19 16:22:26 +0100

    2 seconds

--- Assets/Scripts/Dialogue/DialoguePlayer.cs
+++ Assets/Scripts/Dialogue/DialoguePlayer.cs
@@ -61,7 +61,7 @@ public class DialoguePlayer : MonoBehaviour
 			currentLine = 0;
 			animatorScript.talkingBool = false;
 		} else {
-			yield return new WaitForSeconds(4f);
+			yield return new WaitForSeconds(2f);
 			StartCoroutine(CallLine());
 		}
 	}
